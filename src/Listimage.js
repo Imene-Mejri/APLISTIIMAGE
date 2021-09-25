107a0538f449d3cb68311bf4bb1ddb476256b091
core.pager=cat <mejriimene501@gmail.com>
2021-09-25 15:02:03 +0300

    final edit

--- src/Listimage.js
+++ src/Listimage.js
@@ -1,31 +1,34 @@
 import React from 'react'
-import {useEffect,useState} from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 
 export default function Listimage() {
-    const [image,setimage]=useState([])
+    const [image, setimage] = useState([])
     useEffect(() => {
-      
-        axios.get("https://jsonplaceholder.typicode.com/photos")
-        .then(res => setimage(res.data))
-        .catch(err => alert(err))
-      
-    }, [])
-  console.log(image.length)
-    
-    return (
-        <div className='bigcard'>
-           {image.map(el=>{
-               <div className='container'>
-                  
-                  <h3>title:<span>{el.title}</span></h3>
-                     <h3>picture: <img src= {el.url}/></h3>
-                     <h3>picture1: <img src={el.thumbnailUrl}/></h3>
 
-               </div>
-           })}
-            
+        axios.get("https://jsonplaceholder.typicode.com/photos")
+            .then(res => setimage(res.data))
+            .catch(err => alert(err))
+
+    }, [])
+    console.log(image)
+
+    return (
+        
+        <div className='bigcard'>
+            {image.map(el => 
+                <div className='container'>
+                      <h2>{el.id}</h2>
+                     
+
+                    <h3>title:<span>{el.title}</span></h3>
+                    <h3>picture: <img src={el.url} /></h3>
+                    <h3>picture1: <img src={el.thumbnailUrl} /></h3>
+
+                </div>
+            )}
+
         </div>
     )
 }
